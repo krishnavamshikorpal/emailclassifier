816a7d8d1300b4701b3e868b240bbcf26ee67e2e
Krishna vamshi <67283649+krishnavamshikorpal@users.noreply.github.com>
2021-08-02 21:22:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  1 00:27:01 2021
+
+@author: Krishna Vamshi
+"""
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import numpy as np
+import pickle
+
+# load the model from disk
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv=pickle.load(open('tranform.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+    return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
